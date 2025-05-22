3529efd8ed4d5e7cd3913df921140f97a321beed
mariamecly <mariameeeee082@gmail.com>
2025-05-22 10:42:24 +0000

    nouvelle mise a jour

--- backend/reports/permissions.py
+++ backend/reports/permissions.py
@@ -6,5 +6,10 @@ class IsEtudiantOrAlumni(BasePermission):
         role = getattr(request.user, 'role', None)
         print("Rôle dans IsEtudiantOrAlumni :", role)
         return request.user.is_authenticated and role in [Role.ETUDIANT, Role.ALUMNI]
+class IsAdmin(BasePermission):
+    def has_permission(self, request, view):
+        role = getattr(request.user, 'role', None)
+        print("Rôle dans IsAdmin :", role)
+        return request.user.is_authenticated and role == Role.ADMIN
 
 

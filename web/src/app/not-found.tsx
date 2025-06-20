44ab99116f4f243050213be19b1a9ab9dfee3e11
fkx-69 <125918024+fkx-69@users.noreply.github.com>
2025-06-20 17:49:40 +0000

    Add global not-found page

---
+++ web/src/app/not-found.tsx
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="h-screen flex flex-col items-center justify-center bg-base-100 mx-auto max-w-7xl px-4">
+      <h1 className="text-4xl font-bold mb-8">Page introuvable</h1>
+      <Link href="/" className="btn btn-primary btn-lg">
+        Retour à l'accueil
+      </Link>
+    </div>
+  );
+}

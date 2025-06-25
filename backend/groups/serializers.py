406f9a5d45ccd3d7766c2130d0ce972a62c23b64
mariamecly <mariameeeee082@gmail.com>
2025-06-25 15:31:11 +0000

    new update

--- backend/groups/serializers.py
+++ backend/groups/serializers.py
@@ -12,16 +12,26 @@ class GroupeSerializer(serializers.ModelSerializer):
         read_only=True
     )
     est_membre = serializers.SerializerMethodField()
+    role = serializers.SerializerMethodField()  # 👈 nouveau champ
 
     class Meta:
         model = Groupe
-        fields = ['id', 'nom_groupe', 'description', 'createur', 'membres', 'date_creation', 'est_membre']
+        fields = ['id', 'nom_groupe', 'description', 'createur', 'membres', 'date_creation', 'est_membre', 'role']
 
     def get_est_membre(self, obj):
         request = self.context.get('request')
         if request and request.user.is_authenticated:
             return obj.membres.filter(id=request.user.id).exists()
         return False
+
+    def get_role(self, obj):
+        request = self.context.get('request')
+        if request and request.user.is_authenticated:
+            if obj.createur == request.user:
+                return 'createur'
+            elif obj.membres.filter(id=request.user.id).exists():
+                return 'membre'
+        return None
 
 class MessageSerializer(serializers.ModelSerializer):
     auteur = serializers.ReadOnlyField(source='auteur.username')

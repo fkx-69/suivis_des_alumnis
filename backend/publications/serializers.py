6bc2a917fcdee3fff6ca3b35beb5dfc7d629e0b1
mariamecly <mariameeeee082@gmail.com>
2025-06-25 14:25:40 +0000

    update de publications

--- backend/publications/serializers.py
+++ backend/publications/serializers.py
@@ -4,20 +4,20 @@ from .models import Publication, Commentaire
 class CommentaireSerializer(serializers.ModelSerializer):
     auteur_username = serializers.ReadOnlyField(source='auteur.username')
     auteur_photo_profil = serializers.ImageField(source='auteur.photo_profil', read_only=True)
-    nombres_commentaires= serializers.SerializerMethodField()
-
+    
     class Meta:
         model = Commentaire
         fields = ['id', 'publication', 'auteur_username','auteur_photo_profil', 'contenu', 'date_commentaire']
-        
-    def get_nombre_commentaires(self, obj):
-        return obj.commentaires.count()
+    
 
 class PublicationSerializer(serializers.ModelSerializer):
     auteur_username = serializers.ReadOnlyField(source='auteur.username')
     auteur_photo_profil = serializers.ImageField(source='auteur.photo_profil', read_only=True)
     commentaires = CommentaireSerializer(many=True, read_only=True)
+    nombres_commentaires = serializers.SerializerMethodField()
 
     class Meta:
         model = Publication
-        fields = ['id', 'auteur_username','auteur_photo_profil', 'texte', 'photo', 'video', 'date_publication', 'commentaires']
+        fields = ['id', 'auteur_username','auteur_photo_profil', 'texte', 'photo', 'video', 'date_publication', 'commentaires','nombres_commentaires']
+    def get_nombres_commentaires(self, obj):
+        return obj.commentaires.count()
